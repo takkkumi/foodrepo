83bbcff87d1e946125c70a46d02bd13be0a0c952
takkkumi <nenetyankawwaii@gmail.com>
2019-10-18 11:46:16 +0000

    googleLoginLogout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-import React from "react"
-import { Grid, Divider } from "semantic-ui-react"
-import Header from "./components/Header/Header"
+import React, { useState, useEffect, createContext } from "react"
+import { Grid, Container } from "semantic-ui-react"
+
 import HomePage from "./components/Home/HomePage"
 import SideBar from "./components/SideBar/SideBar"
 
-function App() {
-	return (
-		<Grid>
-			<Grid.Row>
-				<Header />
-			</Grid.Row>
+import firebase from "./config/firebase-config"
+import Navbar from "./components/Header/Navbar"
+export const UserContext = createContext()
+const App = () => {
+	const [user, setUser] = useState(null)
+	const [isLogin, setIsLogin] = useState(false)
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(currentUser => {
+			if (currentUser) {
+				setUser(currentUser)
+				setIsLogin(true)
+			} else {
+				setUser(null)
+				setIsLogin(false)
+			}
+		})
+	}, [isLogin, user])
 
-			<Divider />
-			<Grid.Row>
-				<Grid.Column width={3}>
-					<SideBar />
-				</Grid.Column>
-				<Grid.Column width={13}>
-					<HomePage />
-				</Grid.Column>
-			</Grid.Row>
-		</Grid>
+	return (
+		<UserContext.Provider value={{ user, isLogin }}>
+			<Container>
+				<Grid>
+					<Grid.Row>
+						<Navbar />
+					</Grid.Row>
+
+					<Grid.Row>
+						<Grid.Column width={3}>
+							<SideBar />
+						</Grid.Column>
+						<Grid.Column width={13}>
+							<HomePage />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Container>
+		</UserContext.Provider>
 	)
 }
 
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from "react"
+import { Header, Image, Menu, Container, Dropdown } from "semantic-ui-react"
+import { UserContext } from "../../App"
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const Navbar = () => {
+	const auth = useContext(UserContext)
+	const user = auth.user
+	const googleLogin = () => {
+		const provider = new firebase.auth.GoogleAuthProvider()
+		firebase.auth().signInWithPopup(provider)
+	}
+	const SignOut = () => {
+		firebase.auth().signOut()
+	}
+	return (
+		<Container>
+			<Header as="h2" color="orange">
+				{user ? (
+					<>
+						Hello {user.displayName}{" "}
+						<Image size="mini" src={user.photoURL} verticalAlign="top" />
+					</>
+				) : (
+					"Hello"
+				)}
+
+				<Menu fluid tabular>
+					<Menu.Menu position="right">
+						<Dropdown text="Setting" item simple>
+							<Dropdown.Menu>
+								{!auth.isLogin ? (
+									<Dropdown.Item onClick={() => googleLogin()}>
+										LogIn
+									</Dropdown.Item>
+								) : (
+									<Dropdown.Item onClick={() => SignOut()}>
+										LogOut
+									</Dropdown.Item>
+								)}
+							</Dropdown.Menu>
+						</Dropdown>
+					</Menu.Menu>
+				</Menu>
+			</Header>
+		</Container>
+	)
+}
+
+export default Navbar
